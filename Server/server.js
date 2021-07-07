bf3f9198df8fa76a8e3e936a8164a368fa99d764
mlemke24 <mattrlemke@gmail.com>
2021-07-07 18:56:37 -0500

    server folder started

---
+++ Server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.end.MONGODB_URI || 'mongodb://localhost/lets-brew', {
+    useFindandModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`))
+
