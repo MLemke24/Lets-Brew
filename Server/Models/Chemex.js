531cfd7635b1157da46547ff6812ded768ee4d69
mlemke24 <mattrlemke@gmail.com>
2021-07-10 11:06:23 -0500

    attempting to seed

---
+++ Server/Models/Chemex.js
@@ -0,0 +1,24 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ChemexSchema = new Schema(
+    {
+        id: {
+            type: INTEGER,
+        },
+        cupSize: {
+            type: INTEGER,
+        },
+        grindSize: {
+            type: STRING,
+        },
+        coffeeAmount: {
+            type: INTEGER
+        },
+        water: {
+            type: INTEGER
+        }
+    }
+)
+
+const CHEMEX = mongoose.model('Chemex', ChemexSchema, 'Chemex');
+module.exports = Chemex;
---
+++ Server/seeds/seeds.js
@@ -0,0 +1,69 @@
+import seeder from 'mongoose-seed';
+
+const db = 'mongodb://localhost:3001/lets-brew'
+
+seeder.connect(db, function () {
+    seeder.loadModels([
+        './Chemex.model'
+    ]);
+    seeder.clearModels(['Chemex']);
+    seeder.populateModels(data, function (err, done) {
+        if (err) {
+            return console.log('seed err', err)
+        }
+        if (done) {
+            return console.log('seed done', done)
+        }
+        seeder.disconnect()
+    })
+});
+
+const data = [
+   { 
+    model: 'Chemex',
+    documents: [
+        {
+            'id': '1',
+            'cupSize': '1',
+            'grindSize': 'medium',
+            'coffeeAmount': '25',
+            'water': '340'
+        },
+        {
+            'id': '2',
+            'cupSize': '2',
+            'grindSize': 'medium',
+            'coffeeAmount': '50',
+            'water': '640'
+        },
+        {
+            'id': '3',
+            'cupSize': '3',
+            'grindSize': 'medium',
+            'coffeeAmount': '75',
+            'water': '980'
+        },
+        {
+            'id': '4',
+            'cupSize': '4',
+            'grindSize': 'medium',
+            'coffeeAmount': '100',
+            'water': '1020'
+        },
+        {
+            'id': '5',
+            'cupSize': '5',
+            'grindSize': 'medium',
+            'coffeeAmount': '125',
+            'water': '1360'
+        },
+        {
+            'id': '6',
+            'cupSize': '6',
+            'grindSize': 'medium',
+            'coffeeAmount': '150',
+            'water': '1700'
+        }
+    ]
+}
+]
