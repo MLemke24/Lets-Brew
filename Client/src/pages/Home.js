5599382a23613b2bbf62e366553334d1476f5f35
josh mitchell <jshmtchll@gmail.com>
2021-07-15 16:08:09 -0500

    add icon files/home page started

---
+++ Client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'materialize-css/dist/css/materialize.min.css'
+
+const Home = () => {
+  
+
+  return (
+    <main>
+      
+    </main>
+  );
+};
+
+export default Home;
