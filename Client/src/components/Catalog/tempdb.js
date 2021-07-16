85ffc861cf044e46b4b57738df3db5ad7a088425
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-16 18:16:18 -0500

    all image links are functional, app looks good :)

--- Client/src/components/Catalog/tempdb.js
+++ Client/src/components/Catalog/tempdb.js
@@ -3,7 +3,8 @@ const brews = [
     {
         id: 1,
         cups: 2,
-        name: "french press",
+        name: "French Press",
+        image: require("../../images/icons/French Press/SVG/French Press.svg").default,
         grind: "coarse",
         water: "50 liters",
         weight: "1 megaton",
@@ -16,7 +17,8 @@ const brews = [
     {
         id: 2,
         cups: 4,
-        name: "italian press",
+        name: "Hairo",
+        image: require("../../images/icons/Hairo Vaccum/SVG/Hairo Vaccum .svg").default,
         grind: "fine",
         water: "1 liter",
         weight: "420 g",
@@ -29,15 +31,90 @@ const brews = [
     {
         id: 3,
         cups: 1,
-        name: "german press",
+        name: "Aero Press",
+        image: require("../../images/icons/AeroPress/SVG/AeroPress.svg").default,
         grind: "lite",
         water: "2 mililiters",
         weight: "1 terabite",
-        strength: "strong as your coding game",
+        strength: "steamy strong",
         step1:"boil the coffee",
         step2:"take a walk outside",
         step3:"put down the fire",
         step4:"fraud the insurance company"
+    },
+    {
+        id: 4,
+        cups: 1,
+        name: "Bialetti",
+        image: require("../../images/icons/Bialetti/SVG/Bialetti.svg").default,
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
+        strength: "heavy metal coffee",
+        step1:"boil the coffee",
+        step2:"take a walk outside",
+        step3:"put down the fire",
+        step4:"fraud the insurance company"
+    }
+    ,
+    {
+        id: 5,
+        cups: 1,
+        name: "Chemex",
+        image: require("../../images/icons/Chemex/SVG/Chemex.svg").default,
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
+        strength: "boldness is my second name",
+        step1:"boil the coffee",
+        step2:"take a walk outside",
+        step3:"put down the fire",
+        step4:"fraud the insurance company"
+    }
+    ,
+    {
+        id: 6,
+        cups: 1,
+        name: "Neldrip",
+        image: require("../../images/icons/Nel-Drip/SVG/Nel Drip.svg").default,
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
+        strength: "a very bold brew",
+        step1:"boil the coffee",
+        step2:"take a walk outside",
+        step3:"put down the fire",
+        step4:"fraud the insurance company"
+    }
+    ,
+    {
+        id: 7,
+        cups: 1,
+        name: "V60",
+        image: require("../../images/icons/V60/SVG/V60.svg").default,
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
+        strength: "strong and ready to kick some butt",
+        step1:"boil the coffee",
+        step2:"take a walk outside",
+        step3:"put down the fire",
+        step4:"fraud the insurance company"
+    }
+    ,
+    {
+        id: 8,
+        cups: 1,
+        name: "Kettle",
+        image: require("../../images/icons/Kettle/SVG/Kettle.svg").default,
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
+        strength: "strong as your coding game",
+        step1:"put water in the kettle",
+        step2:"heat until steam comes out of top",
+        step3:"pour water on plastic cup",
+        step4:"remember the alamo"
     }
 ]
 
