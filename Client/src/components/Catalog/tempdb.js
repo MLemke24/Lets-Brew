53de900bf8b8bfdf5151f4ee33bc51ab156f0479
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-16 15:10:12 -0500

    instructions completed

--- Client/src/components/Catalog/tempdb.js
+++ Client/src/components/Catalog/tempdb.js
@@ -4,18 +4,27 @@ const brews = [
         id: 1,
         cups: 2,
         name: "french press",
+        grind: "coarse",
+        water: "50 liters",
+        weight: "1 megaton",
         strength: "saitama strong"
     },
     {
         id: 2,
         cups: 4,
         name: "italian press",
+        grind: "fine",
+        water: "1 liter",
+        weight: "420 g",
         strength: "like a punch trough time"
     },
     {
         id: 3,
         cups: 1,
         name: "german press",
+        grind: "lite",
+        water: "2 mililiters",
+        weight: "1 terabite",
         strength: "strong as your coding game"
     }
 ]
--- Client/src/components/Instructions/index.js
+++ Client/src/components/Instructions/index.js
@@ -15,6 +15,35 @@ const Instructions = () => {
                 <h2>Strenght : </h2>
                 <h4>{Data.filter(brew => brew.name === brewId).map(filterBrew=> filterBrew.strength)}</h4>
             </div>
+            <div>
+                <h2>Volume</h2>
+                <ul>
+                    <li>Water <span>{Data.filter(brew => brew.name === brewId).map(filterBrew=> filterBrew.water)}</span></li>
+                    <li>Grind <span>{Data.filter(brew => brew.name === brewId).map(filterBrew=> filterBrew.grind)}</span></li>
+                    <li>Coffee <span>{Data.filter(brew => brew.name === brewId).map(filterBrew=> filterBrew.weight)}</span></li>
+                </ul>
+            </div>
+            <div>
+                <h2>Steps</h2>
+                <ul>
+                    <li>
+                        <h3>Step One</h3>
+                        <p>description of step 1 for {brewId}</p>
+                    </li>
+                    <li>
+                        <h3>Step Two</h3>
+                        <p>description of step 2 for {brewId}</p>
+                    </li>
+                    <li>
+                        <h3>Step Three</h3>
+                        <p>description of step 3 for {brewId}</p>
+                    </li>
+                    <li>
+                        <h3>Step Four</h3>
+                        <p>description of step 4 for {brewId}</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
