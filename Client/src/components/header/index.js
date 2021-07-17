3863be4e1d5d57bcdd66102b945e37dd7b290fbf
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-17 00:09:50 -0500

    logout implemented

--- Client/src/components/header/index.js
+++ Client/src/components/header/index.js
@@ -2,8 +2,59 @@ import React from "react";
 import 'materialize-css/dist/css/materialize.min.css'
 import { Link } from "react-router-dom";
 import logo from "../../images/icons/Logo/SVG/Logo.svg"
+/// we need to add an "if logged in" for the logout button
+import Auth from "../../utils/auth"
 
 const Header = () => {
+
+  const LoggedInNav = () => {
+    if (Auth.loggedIn()) {
+      <>
+        <ul id="nav-mobile" className="left">
+          <li>
+            <a href="/" onClick={()=> Auth.logout()}>Logout</a>
+          </li>
+          <li>
+            <Link to="/signup">Sign-Up</Link>
+          </li>
+        </ul>
+      </>
+    } else {
+      return (
+        <>
+        {Navigation()}
+        </>
+      )
+    }
+  }
+
+  const Navigation = () => {
+    return (
+      <>
+        <ul id="nav-mobile" className="left">
+          <li>
+            <Link to="/login">Sign-In</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign-Up</Link>
+          </li>
+        </ul>
+
+
+        <ul id="nav-mobile" className="right">
+          <li>
+            <a href="sass.html">Get-Started</a>
+          </li>
+          <li>
+            <a href="badges.html">Shop</a>
+          </li>
+        </ul>
+      </>
+
+    )
+  }
+
+
   return (
     <nav>
       <div className="nav-wrapper">
@@ -22,22 +73,9 @@ const Header = () => {
             </h1>
           </a>
         </Link>
-        <ul id="nav-mobile" className="left">
-          <li>
-            <Link to = "/login">Sign-In</Link>
-          </li>
-          <li>
-            <Link to = "/signup">Sign-Up</Link>
-          </li>
-        </ul>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <a href="sass.html">Get-Started</a>
-          </li>
-          <li>
-            <a href="badges.html">Shop</a>
-          </li>
-        </ul>
+        <>
+          {LoggedInNav()}
+        </>
       </div>
     </nav>
   );
