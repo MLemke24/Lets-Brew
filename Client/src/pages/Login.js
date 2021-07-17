2101468948ea282f1ea2d868179d55b298a06764
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-16 22:32:01 -0500

    mutations, login and sign up forms are ready, awaiting back end ( all sourced from module 22 )

--- Client/src/pages/Login.js
+++ Client/src/pages/Login.js
@@ -4,14 +4,14 @@ import  {LOGIN } from '../utils/mutations'
 import Auth from '../utils/auth'
 
 const LoginForm = () => {
-    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [formState, setFormState] = useState({ username: '', password: '' });
     const [login, { error }] = useMutation(LOGIN);
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         try {
             const mutationResponse = await login({
-                variables: { email: formState.email, password: formState.password },
+                variables: { username: formState.username, password: formState.password },
             });
             const token = mutationResponse.data.login.token;
             Auth.login(token);
--- Client/src/utils/mutations.js
+++ Client/src/utils/mutations.js
@@ -4,8 +4,8 @@ import { gql } from '@apollo/client'
 // these are placeholder mutations sourced from module.22
 //////////////////////
 export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       token
       user {
         _id
@@ -16,15 +16,11 @@ export const LOGIN = gql`
 
 export const ADD_USER = gql`
   mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
+    $username: String!
     $password: String!
   ) {
     addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
+      username: $username
       password: $password
     ) {
       token
