39072846b94271c5198e1117291dacf89e9ff930
Charles <cchester1@angelo.edu>
2021-07-20 09:13:50 -0500

    Brew component

--- Client/src/components/Cards/Brew.js
+++ Client/src/components/Cards/Brew.js
@@ -1,7 +1,13 @@
 import React, {useState} from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_BREW } from '../../utils/queries';
 import cupButton from '../cupButton/cupButton'
 
+// create the BrewSpecs function to export our component
+// use state to dynamically change the instruction values 
+// use the QUERY_BREW to pass our data into our state 
 const BrewSpecs = ({ data }) => {
+    const [brew] = useQuery(QUERY_BREW)
     const [cupSize, setCupSize] = useState('')
     const [grindSize, setGrindSize] = useState(1)
     const [coffeeAmount, setCoffeeAmount] = useState(1)
@@ -14,10 +20,10 @@ const BrewSpecs = ({ data }) => {
   // create a function to pass into a button (useEffect function)
   // the button will render the data in the return below
   const renderData = () => {
-    setCupSize(data.cupSize)
-    setGrindSize(data.grindSize)
-    setCoffeeAmount(data.coffeeAmount)
-    setWater(data.water)
+    setCupSize(brew.cupSize)
+    setGrindSize(brew.grindSize)
+    setCoffeeAmount(brew.coffeeAmount)
+    setWater(brew.water)
   }
 
     return (
@@ -27,7 +33,6 @@ const BrewSpecs = ({ data }) => {
         <p>{coffeeAmount}</p>
         <p>{water}</p>
         <cupButton onChange={renderData()}/>
-        <button type="submit" onSubmit={renderData()}></button>
       </div>
   )
 }
