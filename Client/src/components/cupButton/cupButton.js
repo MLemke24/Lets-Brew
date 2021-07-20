2d51bdcafe1dc4dc8c176ac643f90f2e89a6d0df
Charles <cchester1@angelo.edu>
2021-07-20 10:40:28 -0500

    Brew components modularized, front end queries written per each resolver, and components imported into instructions file

--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react' 
+import React, { useState } from 'react'
+//could use params here in the place of the data prop
 
 const cupButton = ({ data }) => {
   // const [num, setNum] = useState(1)
 
   const handleNum = () => {
     setNum(data.id)
-  } 
+  }
 
   return (
     <input type="submit" onChange={handleNum}>{num}</input>
