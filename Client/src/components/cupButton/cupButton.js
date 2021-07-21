7540437f40006bb5e8be01749ba401670f68cb8d
Charles <cchester1@angelo.edu>
2021-07-20 20:12:19 -0500

    ..

--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
-//could use params here in the place of the data prop
+// import { AeroPress, BeeHouse, Chemex, FrenchPress, MokaPot, Siphon, V60, Wave } from '../../../../server/Models/index'
+import  { AeroPress, BeeHouse, Chemex, FrenchPress, MokaPot, Siphon, V60, Wave } from '../../utils/queries';
+import { useQuery } from '@apollo/client'
+import { QUERY_AEROPRESS, QUERY_BEEHOUSE, QUERY_CHEMEX, QUERY_FRENCHPRESS, QUERY_MOKAPOT, QUERY_SIPHON, QUERY_V60, QUERY_WAVE } from '../../utils/queries';
 
 const cupButton = (props) => {
-  
+  const {brew} = useQuery(QUERY_CHEMEX)
+  const [brewData, setBrewData] = useState({cupSize: 1})
+
+  return (
+    <button type="submit">{brew.cupSize}</button>
+  )
 }
 
 export default cupButton;
