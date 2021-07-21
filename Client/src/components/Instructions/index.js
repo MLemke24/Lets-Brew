cf6185669c0ae16bf7447de00b1c065c073d1653
josh mitchell <jshmtchll@gmail.com>
2021-07-20 20:57:32 -0500

    hide instructions component

--- Client/src/components/Instructions/index.js
+++ Client/src/components/Instructions/index.js
@@ -4,22 +4,25 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Timer from "../Timer";
 import Data from "../../db/tempdb";
-import { Link } from "react-router-dom";
-import CupButton from '../cupButton/cupButton'
+//import { Link } from "react-router-dom";
+import CupButton from "../cupButton/cupButton";
 // import { AeroPress, BeeHouse, Chemex, FrenchPress, MokaPot, Siphon, V60, Wave } from '../Cards';
 // make a button for notes? so you can share them???
 /// make a button to add it to favorites????
 
-
 const Instructions = () => {
   const { brewId } = useParams();
   // const [ cups, setCups ] = useState
-  const GoNotes = () => {
-      return ( <Link to={`/notes/${brewId}`}><button>Go Notes!</button></Link>)
-  }
+//   const GoNotes = () => {
+//     return (
+//       <Link to={`/notes/${brewId}`}>
+//         <button>Go Notes!</button>
+//       </Link>
+//     );
+//   };
 
   // const cups = function(num, ) {
-    
+
   // }
 
   return (
@@ -27,70 +30,74 @@ const Instructions = () => {
       {Data.filter((brew) => brew.name === brewId).map((filterBrew) => {
         return (
           <>
-            <div className="instruction-title">
+            <div id="showDiv" className="instruction-title">
               <h1> Brewing : {brewId} </h1>
-              <CupButton/>
-              <Timer />
-              <GoNotes/>
+              <CupButton />
             </div>
 
-            <div className="instruction-content">
-              <div>
-                <div>
-                  <h2>Strength : </h2>
-                  <h4>{filterBrew.strength}</h4>
-                </div>
-                <h2>Volume:</h2>
-                <ul>
-                  <li>
-                    <div className="btmborder">
-                      <span className="ingredients">WATER </span>
-                      <span className="measurements">
-                        {" "}
-                        {filterBrew.water}
-                      </span>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="btmborder">
-                      <span className="ingredients">GRIND </span>
-                      <span className="measurements">
-                        {" "}
-                        {filterBrew.grind}
-                      </span>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="btmborder">
-                      <span className="ingredients">COFFEE </span>
-                      <span className="measurements">
-                        {" "}
-                        {filterBrew.weight}
-                      </span>
-                    </div>
-                  </li>
-                </ul>
+            <div id="hideDiv" style={{display: 'none'}}>
+              <div className="instruction-title">
+                <Timer />
               </div>
-              <div className="steps-content">
-                <h2>STEPS</h2>
-                <ul>
-                  <li className="step-line-height">
-                    <h4>Step One:</h4>
-                    <p>{filterBrew.step1}</p>
-                  </li>
-                  <li className="step-line-height pad">
-                    <h4>Step Two:</h4>
-                    <p>{filterBrew.step2}</p>
-                  </li>
-                  <li className="step-line-height pad">
-                    <h4>Step Three:</h4>
-                    <p>{filterBrew.step3}</p>
-                  </li>
-                  <li className="step-line-height pad">
-                    <h4>Step Four:</h4>
-                    <p>{filterBrew.step4}</p>
-                  </li>
-                </ul>
+
+              <div className="instruction-content">
+                <div>
+                  <div>
+                    <h2>Strength : </h2>
+                    <h4>{filterBrew.strength}</h4>
+                  </div>
+                  <h2>Volume:</h2>
+                  <ul>
+                    <li>
+                      <div className="btmborder">
+                        <span className="ingredients">WATER </span>
+                        <span className="measurements">
+                          {" "}
+                          {filterBrew.water}
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="btmborder">
+                        <span className="ingredients">GRIND </span>
+                        <span className="measurements">
+                          {" "}
+                          {filterBrew.grind}
+                        </span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className="btmborder">
+                        <span className="ingredients">COFFEE </span>
+                        <span className="measurements">
+                          {" "}
+                          {filterBrew.weight}
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="steps-content">
+                  <h2>STEPS</h2>
+                  <ul>
+                    <li className="step-line-height">
+                      <h4>Step One:</h4>
+                      <p>{filterBrew.step1}</p>
+                    </li>
+                    <li className="step-line-height pad">
+                      <h4>Step Two:</h4>
+                      <p>{filterBrew.step2}</p>
+                    </li>
+                    <li className="step-line-height pad">
+                      <h4>Step Three:</h4>
+                      <p>{filterBrew.step3}</p>
+                    </li>
+                    <li className="step-line-height pad">
+                      <h4>Step Four:</h4>
+                      <p>{filterBrew.step4}</p>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </>
--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -1,30 +1,41 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback } from "react";
 //could use params here in the place of the data prop
-import mug from '../../images/icons/Mug/1x/Mug.png'
-export default function App ()  {
+import mug from "../../images/icons/Mug/1x/Mug.png";
+import { hideInstructions } from "../../utils/wheel";
+export default function App() {
   // const [num, setNum] = useState(1)
   const [count, setCount] = useState(0);
 
-  const OnIncrementClick = useCallback((e) => {
-    setCount(count + 1);
-  }, [count])
+  const OnIncrementClick = useCallback(
+    (e) => {
+      setCount(count + 1);
+    },
+    [count]
+  );
 
-  const OnDecrementClick = useCallback((e) => {
-    setCount(count - 1);
-  }, [count])
+  const OnDecrementClick = useCallback(
+    (e) => {
+      setCount(count - 1);
+    },
+    [count]
+  );
 
   return (
     <div>
-      <img id='mug' src={mug} alt='mug' />
+      <img id="mug" src={mug} alt="mug" />
       <div className="DisplayDiv">
-                <p>Counter</p>
-                {count}
-            </div>
-            <div className="ButtonDiv">
-                <div onClick={OnIncrementClick} className="Button">Increment</div>
-                <div onClick={OnDecrementClick} className="Button">Decrement</div>
-                <button>Enter</button>
-            </div>
+        <h4>How many cups?</h4>
+        <div>{count}</div>
+      </div>
+      <div className="ButtonDiv">
+        <div onClick={OnIncrementClick} className="cup-sel">
+          <div className="btntext">+</div>
+        </div>
+        <div onClick={OnDecrementClick} className="cup-sel">
+          <div className="btntext">-</div>
+        </div>
+        <button onClick={hideInstructions}>Enter</button>
+      </div>
     </div>
-  )
+  );
 }
