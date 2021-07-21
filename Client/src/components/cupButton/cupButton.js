775d28d01b44fd2cba142c3b7ef1dbc990d01c93
josh mitchell <jshmtchll@gmail.com>
2021-07-21 15:09:17 -0500

    cup page style

--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -2,6 +2,8 @@ import React, { useState, useCallback } from "react";
 //could use params here in the place of the data prop
 import mug from "../../images/icons/Mug/1x/Mug.png";
 import { hideInstructions } from "../../utils/wheel";
+import plus from "../../images/icons/plus.png"
+import minus from "../../images/icons/minus.png"
 export default function App() {
   // const [num, setNum] = useState(1)
   const [count, setCount] = useState(0);
@@ -21,20 +23,23 @@ export default function App() {
   );
 
   return (
-    <div>
-      <img id="mug" src={mug} alt="mug" />
+    <div className="cupselection column">
+      <img className="cuplogo" id="mug" src={mug} alt="mug" />
       <div className="DisplayDiv">
         <h4>How many cups?</h4>
-        <div>{count}</div>
+        <div className="count">{count}</div>
       </div>
-      <div className="ButtonDiv">
-        <div onClick={OnIncrementClick} className="cup-sel">
-          <div className="btntext">+</div>
+      <div className="ButtonDiv row">
+        <div onClick={OnIncrementClick} className="">
+        <img className="plus" src={plus} alt="plus"></img>
         </div>
-        <div onClick={OnDecrementClick} className="cup-sel">
-          <div className="btntext">-</div>
+        <div onClick={OnDecrementClick} className="">
+        <img className="minus" src={minus} alt="minus"></img>
         </div>
-        <button onClick={hideInstructions}>Enter</button>
+        
+      </div>
+      <div className="cupbtn">
+      <div onClick={hideInstructions} className="btn"> SUBMIT</div>
       </div>
     </div>
   );
