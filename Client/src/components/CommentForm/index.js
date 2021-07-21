028f625e63e10a01e777274caea0e91d3b489486
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-21 18:28:00 -0500

    form input completed

--- Client/src/components/CommentForm/index.js
+++ Client/src/components/CommentForm/index.js
@@ -2,40 +2,20 @@ import React, { useState } from 'react';
 
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_POST } from '../../utils/mutations';
-import { QUERY_POST, QUERY_ME } from '../../utils/queries';
+
 //this expects a title
 const ThoughtForm = () => {
-  const [formState, setText] = useState({ postText: '', postTitle: ''});
-  const [addPost] = useMutation(ADD_POST, {
-    update(cache, { data: { addPost } }) {
-      try {
-        // update thought array's cache
-        // could potentially not exist yet, so wrap in a try/catch
-        const { posts } = cache.readQuery({ query: QUERY_POST });
-        cache.writeQuery({
-          query: QUERY_POST,
-          data: { posts: [...posts, addPost] }
-        });
-      } catch (e) {
-        console.error(e);
-      }
-
-      // update me object's cache
-      const { me } = cache.readQuery({ query: QUERY_ME });
-      cache.writeQuery({
-        query: QUERY_ME,
-        data: { me: { ...me, posts: [...me.posts, addPost] } }
-      });
-    }
-  });
+  const [formState, setFormState] = useState({ postText: '', postTitle: ''});
+  const [addPost, {error}] = useMutation(ADD_POST);
 
   // update state based on form input changes
   const handleChange = (event) => {
       const { name, value} = event.target;
 
-      setText({
+      setFormState({
         ...formState,
-        [name]:value});
+        [name]:value,
+      });
 
       
   };
@@ -44,7 +24,7 @@ const ThoughtForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    try {
+    
       const mutationResponse = await addPost({
         variables: {
            postTitle: formState.postTitle, 
@@ -54,11 +34,8 @@ const ThoughtForm = () => {
       console.log(mutationResponse)
 
       // clear form value
-      // setText('');
       
-    } catch (e) {
-      console.error(e);
-    }
+    
     
   };
 
@@ -70,23 +47,26 @@ const ThoughtForm = () => {
         onSubmit={handleFormSubmit}
       >
         <input
+          className="form-input"
+          id= "postTitle"
+          name= "postTitle"
           placeholder="enter a Title for your post"
           value= {formState.postTitle}
-          className=""
-          name= "title"
           onChange={handleChange}
-        ></input>
+        />
         <textarea
+          className="form-input"
+          id= "postText"
+          name= "postText"
           placeholder="Here's a new post..."
           value= {formState.postText}
-          className=""
-          name="text"
           onChange={handleChange}
-        ></textarea>
+        />
         <button className="" type="submit">
           Submit
         </button>
       </form>
+      {error && <div>failed submition</div>}
     </div>
   );
 };
