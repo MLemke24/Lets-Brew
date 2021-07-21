87f81e3cf6958c5d035a6aaed8bc3390a7e138b4
Charles <cchester1@angelo.edu>
2021-07-20 19:51:37 -0500

    ..

--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react'
 //could use params here in the place of the data prop
 
 const cupButton = (props) => {
-  // const [num, setNum] = useState(1)
-  // const handleNum = () => {
-  //   setNum(data.id)
-  // }
   
 }
 
