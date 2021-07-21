0cc790540ffd322d46f4ef4b03df61db50c90b69
Charles <cchester1@angelo.edu>
2021-07-20 19:34:04 -0500

    ..

--- Client/src/components/cupButton/cupButton.js
+++ Client/src/components/cupButton/cupButton.js
@@ -1,16 +1,12 @@
 import React, { useState } from 'react'
 //could use params here in the place of the data prop
 
-const cupButton = ({ data }) => {
+const cupButton = (props) => {
   // const [num, setNum] = useState(1)
-
-  const handleNum = () => {
-    setNum(data.id)
-  }
-
-  return (
-    <input type="submit" onChange={handleNum}>{num}</input>
-  )
+  // const handleNum = () => {
+  //   setNum(data.id)
+  // }
+  
 }
 
 export default cupButton;
