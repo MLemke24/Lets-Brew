3000adda577c89da17615a56fddb1a45d5510279
mlemke24 <mattrlemke@gmail.com>
2021-07-22 10:14:30 -0500

    more spelling

--- Server/server.js
+++ Server/server.js
@@ -20,11 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../Client/build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../Client/build/index.html"));
 });
 
 db.once('open', () => {
