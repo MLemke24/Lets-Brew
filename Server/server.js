d4e97e67d8d2a3d3fd6dcc5912e495be4fac7d0a
mlemke24 <mattrlemke@gmail.com>
2021-07-22 09:38:56 -0500

    merging with main!

--- Server/server.js
+++ Server/server.js
@@ -19,6 +19,14 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}`);
