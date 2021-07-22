b6bbe7ed65eeb1f4379a8f0a0cde6abecf932ca5
mlemke24 <mattrlemke@gmail.com>
2021-07-22 13:16:51 -0500

    changed server

--- Server/server.js
+++ Server/server.js
@@ -24,9 +24,10 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 db.once('open', () => {
     app.listen(PORT, () => {
