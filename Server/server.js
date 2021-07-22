8dc8e068ed387289506c50144e0dfbfa01b0c662
mlemke24 <mattrlemke@gmail.com>
2021-07-22 10:46:38 -0500

    still messing with it

--- Server/server.js
+++ Server/server.js
@@ -20,11 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../Client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once('open', () => {
