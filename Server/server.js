ada8128239d3e4a67ee9103bd07fee18091b17b3
mlemke24 <mattrlemke@gmail.com>
2021-07-22 13:48:11 -0500

    almost there

--- Server/server.js
+++ Server/server.js
@@ -21,13 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../Client/build')));
 }
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
