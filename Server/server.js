d780762ad181d6a6a96dc4a823edc2df6bbd77fe
mlemke24 <mattrlemke@gmail.com>
2021-07-22 12:18:38 -0500

    maybe fixed

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs, resolvers } = require('./Schemas')
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
